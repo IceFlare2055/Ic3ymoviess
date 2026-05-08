6c73574948312161d47b28350c7f8d345dc98037
IceFlare2055 <138256686+IceFlare2055@users.noreply.github.com>
2026-05-08 10:45:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,6 +13,17 @@ if (window.location.hostname === 'localhost' || window.location.hostname === '12
 export const img = (path, size = 'w342') => path ? `https://wsrv.nl/?url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2F${size}${encodeURIComponent(path)}&output=webp&q=80&n=-1` : null;
 
 export const player = {
-  movie: (id) => `https://vidsrc.to/embed/movie/${id}`,
-  tv: (id, s, e) => `https://vidsrc.to/embed/tv/${id}/${s}/${e}`
+  movie: (id, opts = {}) => buildVid(`https://www.vidking.net/embed/movie/${id}`, opts),
+  tv: (id, s, e, opts = {}) => buildVid(`https://www.vidking.net/embed/tv/${id}/${s}/${e}`, opts),
 };
+
+function buildVid(base, extra = {}) {
+  const p = new URLSearchParams({
+    color: '16FF00',
+    autoPlay: 'true',
+    nextEpisode: 'true',
+    episodeSelector: 'true',
+    ...extra,
+  });
+  return `${base}?${p}`;
+}
